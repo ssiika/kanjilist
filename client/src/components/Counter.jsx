3cd11240d9fba01dc2566fd5f85d9dc3452156ed
Justin Nyilas <justin_nyilas@hotmail.com>
2023-04-16 17:54:48 +1000

    Add counter

--- client/src/components/Counter.jsx
+++ client/src/components/Counter.jsx
@@ -1,6 +1,16 @@
+import { useSelector } from "react-redux";
+
 function Counter() {
+  
+  const {kanjiList} = useSelector((state) => state.kanji)
+
+  const knownSum = kanjiList.reduce(
+    (total, curr) => curr.known === "true" ? total + 1 : total,
+    0
+  )
+
   return (
-    <div className="counterbox">0/2135 (00%)</div>
+    <div className="counterbox">{knownSum}/{kanjiList.length} ({Math.floor( (knownSum / kanjiList.length) * 100 )}%)</div>
   )
 }
 
